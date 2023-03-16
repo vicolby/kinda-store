2e290d0530523b7a8ef60ee4018eff5e77d16f4a
Vi <gimmefear@gmail.com>
2023-03-16 19:58:48 +0600

    add CAS func

--- store.go
+++ store.go
@@ -1,10 +1,29 @@
 package main
 
 import (
+	"crypto/sha1"
+	"encoding/hex"
 	"io"
 	"log"
 	"os"
+	"strings"
 )
+
+func CASPathTransformFunc(key string) string {
+	hash := sha1.Sum([]byte(key))
+	hashStr := hex.EncodeToString(hash[:])
+
+	blocksize := 5
+	sliceLen := len(hashStr) / blocksize
+	paths := make([]string, sliceLen)
+
+	for i := 0; i < sliceLen; i++ {
+		from, to := i*blocksize, (i*blocksize)+blocksize
+		paths[i] = hashStr[from:to]
+	}
+
+	return strings.Join(paths, "/")
+}
 
 type PathTransformFunc func(string) string
 
--- store_test.go
+++ store_test.go
@@ -3,7 +3,16 @@ package main
 import (
 	"bytes"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
+
+func TestPathTransformFunc(t *testing.T) {
+	key := "momsbestpicture"
+	pathname := CASPathTransformFunc(key)
+	expected := "68044/29f74/181a6/3c50c/3d81d/733a1/2f14a/353ff"
+	assert.Equal(t, expected, pathname)
+}
 
 func TestStire(t *testing.T) {
 	opts := StoreOpts{
