f5a1d69dba2506bc8558b3c1e168b82138821e10
Vi <gimmefear@gmail.com>
2023-03-24 20:33:17 +0600

    add server

--- server.go
+++ server.go
@@ -1,1 +1,23 @@
 package main
+
+type FileServerOpts struct {
+	ListenAddr        string
+	StorageRoot       string
+	PathTransformFunc PathTransformFunc
+}
+
+type FileServer struct {
+	FileServerOpts
+	Store *Store
+}
+
+func NewFileServer(opts FileServerOpts) *FileServer {
+	storeOpts := StoreOpts{
+		Root:              opts.StorageRoot,
+		PathTransformFunc: opts.PathTransformFunc,
+	}
+	return &FileServer{
+		FileServerOpts: opts,
+		Store:          NewStore(storeOpts),
+	}
+}
