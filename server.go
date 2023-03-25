f25d1a67af78af7707668034430840f6c09cd795
Vi <gimmefear@gmail.com>
2023-03-25 22:25:55 +0600

    add listening loop and stop func

--- server.go
+++ server.go
@@ -1,14 +1,23 @@
 package main
 
+import (
+	"fmt"
+	"log"
+
+	"github.com/vicolby/kinda-store/p2p"
+)
+
 type FileServerOpts struct {
-	ListenAddr        string
 	StorageRoot       string
 	PathTransformFunc PathTransformFunc
+	Transport         *p2p.TCPTransport
 }
 
 type FileServer struct {
 	FileServerOpts
 	Store *Store
+
+	quitch chan struct{}
 }
 
 func NewFileServer(opts FileServerOpts) *FileServer {
@@ -19,5 +28,35 @@ func NewFileServer(opts FileServerOpts) *FileServer {
 	return &FileServer{
 		FileServerOpts: opts,
 		Store:          NewStore(storeOpts),
+		quitch:         make(chan struct{}),
 	}
+}
+
+func (f *FileServer) Stop() {
+	close(f.quitch)
+}
+
+func (f *FileServer) loop() {
+	defer func() {
+		log.Println("Closing file server")
+		f.Transport.Close()
+	}()
+
+	for {
+		select {
+		case <-f.quitch:
+			return
+		case msg := <-f.Transport.Consumer():
+			fmt.Println(msg)
+		}
+	}
+}
+
+func (f *FileServer) Start() error {
+	if err := f.Transport.ListenAndAccept(); err != nil {
+		return err
+	}
+
+	f.loop()
+	return nil
 }
