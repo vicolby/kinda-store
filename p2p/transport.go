6652dc3aa9c5362aeccfcc8c3e4f685329b9d4bf
Vi <gimmefear@gmail.com>
2023-04-03 23:58:03 +0600

    extend interfaces

--- p2p/transport.go
+++ p2p/transport.go
@@ -1,7 +1,11 @@
 package p2p
 
+import "net"
+
 // Peer is the interface that represents the remote node
 type Peer interface {
+	Send([]byte) error
+	RemoteAddr() net.Addr
 	Close() error
 }
 
@@ -9,6 +13,7 @@ type Peer interface {
 // between the node in network. This can be of the
 // form (TCP, UDP, websockets...)
 type Transport interface {
+	Dial(string) error
 	ListenAndAccept() error
 	Consume() <-chan RPC
 	Close() error
