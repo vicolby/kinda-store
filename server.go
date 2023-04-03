1b3e601b2572c801a23db187287bc6cc67ac15e7
Vi <gimmefear@gmail.com>
2023-04-03 23:57:19 +0600

    add network bootstrap and OnPeer

--- server.go
+++ server.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"sync"
 
 	"github.com/vicolby/kinda-store/p2p"
 )
@@ -11,11 +12,15 @@ type FileServerOpts struct {
 	StorageRoot       string
 	PathTransformFunc PathTransformFunc
 	Transport         *p2p.TCPTransport
+	BootstrapNodes    []string
 }
 
 type FileServer struct {
 	FileServerOpts
 	Store *Store
+
+	peerLock sync.RWMutex
+	peers    map[string]p2p.Peer
 
 	quitch chan struct{}
 }
@@ -29,11 +34,22 @@ func NewFileServer(opts FileServerOpts) *FileServer {
 		FileServerOpts: opts,
 		Store:          NewStore(storeOpts),
 		quitch:         make(chan struct{}),
+		peers:          make(map[string]p2p.Peer),
 	}
 }
 
 func (f *FileServer) Stop() {
 	close(f.quitch)
+}
+
+func (f *FileServer) OnPeer(peer p2p.Peer) error {
+	f.peerLock.Lock()
+	defer f.peerLock.Unlock()
+	f.peers[peer.RemoteAddr().String()] = peer
+
+	log.Printf("New peer: %s", peer.RemoteAddr().String())
+
+	return nil
 }
 
 func (f *FileServer) loop() {
@@ -52,8 +68,27 @@ func (f *FileServer) loop() {
 	}
 }
 
+func (f *FileServer) bootstrapNetwork() error {
+	for _, addr := range f.BootstrapNodes {
+		if len(addr) == 0 {
+			continue
+		}
+		go func(addr string) {
+			fmt.Println("Dialing", addr)
+			if err := f.Transport.Dial(addr); err != nil {
+				log.Printf("Error dialing %s: %s", addr, err)
+			}
+		}(addr)
+	}
+	return nil
+}
+
 func (f *FileServer) Start() error {
 	if err := f.Transport.ListenAndAccept(); err != nil {
+		return err
+	}
+
+	if err := f.bootstrapNetwork(); err != nil {
 		return err
 	}
 
