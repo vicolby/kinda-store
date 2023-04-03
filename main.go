c4e777886fb6a99fc7446991ebc09b8824703061
Vi <gimmefear@gmail.com>
2023-04-03 23:58:16 +0600

    update main

--- main.go
+++ main.go
@@ -2,14 +2,13 @@ package main
 
 import (
 	"log"
-	"time"
 
 	"github.com/vicolby/kinda-store/p2p"
 )
 
-func main() {
+func makeServer(listenAddr string, nodes ...string) *FileServer {
 	tcpTransportOpts := p2p.TCPTransportOpts{
-		ListenAddr:    ":3000",
+		ListenAddr:    listenAddr,
 		HandShakeFunc: p2p.NOPHandshakeFunc,
 		Decoder:       p2p.DefaultDecoder{},
 	}
@@ -17,19 +16,26 @@ func main() {
 	tcpTransport := p2p.NewTCPTransport(tcpTransportOpts)
 
 	fileServerOpts := FileServerOpts{
-		StorageRoot:       "3000_network",
+		StorageRoot:       listenAddr + "_network",
 		PathTransformFunc: CASPathTransformFunc,
 		Transport:         tcpTransport,
+		BootstrapNodes:    nodes,
 	}
 
-	fileServer := NewFileServer(fileServerOpts)
+	s := NewFileServer(fileServerOpts)
+
+	tcpTransport.OnPeer = s.OnPeer
+
+	return s
+}
+
+func main() {
+	s1 := makeServer(":3000", "")
+	s2 := makeServer(":3001", ":3000")
 
 	go func() {
-		time.Sleep(3 * time.Second)
-		fileServer.Stop()
+		log.Fatal(s1.Start())
 	}()
 
-	if err := fileServer.Start(); err != nil {
-		log.Fatal(err)
-	}
+	s2.Start()
 }
