0c4e00b9bd78dd37cb09412c91135e35ce91a6fe
Vi <gimmefear@gmail.com>
2023-04-04 23:55:51 +0600

    add interface injecting

--- p2p/tcp_transport.go
+++ p2p/tcp_transport.go
@@ -9,7 +9,7 @@ import (
 
 type TCPPeer struct {
 	// conn is the underlying connection of the peer
-	conn net.Conn
+	net.Conn
 	// if we dial and retrieve a conn -> outbound == true
 	// if we accept and retrieve a conn -> outbound == false
 	outbound bool
@@ -17,22 +17,14 @@ type TCPPeer struct {
 
 func NewTCPPeer(conn net.Conn, outbound bool) *TCPPeer {
 	return &TCPPeer{
-		conn:     conn,
+		Conn:     conn,
 		outbound: outbound,
 	}
 }
 
 func (p *TCPPeer) Send(b []byte) error {
-	_, err := p.conn.Write(b)
+	_, err := p.Conn.Write(b)
 	return err
-}
-
-func (p *TCPPeer) RemoteAddr() net.Addr {
-	return p.conn.RemoteAddr()
-}
-
-func (p *TCPPeer) Close() error {
-	return p.conn.Close()
 }
 
 type TCPTransportOpts struct {
